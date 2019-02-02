90e8335c3f050fa97e6e41169a643272172bdb68
eeoomm <eeoomm@live.com.mx>
2019-02-02 17:11:41 -0600

    primero

---
+++ Store/routes/products.js
@@ -0,0 +1,33 @@
+const router = require('express').Router()
+const Product = require('../models/Product')
+
+//lista
+router.get('/',(req, res)=>{
+  Product.find({inStock:true})
+    .then(products=>{
+      res.render('products/list',{products})
+    }).catch(e=>res.render('error'))
+})
+
+//detalle
+router.get('/detail/:productId', (req,res)=>{
+  Product.findById(req.params.productId)
+    .then(product=>{
+      res.render('products/detail',product)
+    }).catch(e=>res.render('error'))
+})
+
+//formulario
+router.get('/new', (req, res)=>{
+  res.render('products/form')
+})
+
+router.post('/new',(req, res)=>{
+  console.log(req.body)
+  Product.create(req.body)
+    .then(product=>{
+      res.redirect('/products')
+    }).catch(e=>res.render('error'))
+})
+
+module.exports = router
